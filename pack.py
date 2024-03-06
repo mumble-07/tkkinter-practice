e4b28aa6951eed5f4c2178006a5b89d7d7c12ef3
mumble <andreapauline.deguzman@gmail.com>
2024-03-06 10:50:16 +0800

    learning pack

--- pack.py
+++ pack.py
@@ -14,8 +14,8 @@ button = ttk.Button(window, text = 'Button')
 
 # layout 
 label1.pack(side = 'top', expand = True, fill = 'both', padx = 10, pady = 10)
-label2.pack(side = 'left', expand = True, fill = 'both')
-label3.pack(side = 'top', expand = True, fill = 'both')
+label2.pack(side = 'left', expand = True, fill = 'x', ipadx=10, ipady=20)
+label3.pack(side = 'top', expand = True, fill = 'y')
 button.pack(side = 'top', expand = True, fill = 'both')
 
 # run
